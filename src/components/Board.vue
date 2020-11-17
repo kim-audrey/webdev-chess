536e2e621df72b70393cdc6e3cff2741f833a99a
Michael Papadopoulos <micpap25@gmail.com>
2020-11-17 17:06:28 -0500

    Chessboard Setup Logic, Images, Pieces, Prettier Removed

---
+++ src/components/Board.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="board">
+      <table class="chess-board">
+            <tbody>
+                <tr>
+                    <th></th>
+                    <th>a</th>
+                    <th>b</th>
+                    <th>c</th>
+                    <th>d</th>
+                    <th>e</th>
+                    <th>f</th>
+                    <th>g</th>
+                    <th>h</th>
+                </tr>
+                <tr v-for="n in 8" :key="n">
+                    <th>{{9 - n}}</th>
+                    <td v-for="m in 8" :key="m" :class="{ light: (n+m)%2==0, dark: (n+m)%2==1}" />
+                </tr>
+            </tbody>
+        </table>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            tilesArray: new Array[8][8],
+            pieceArray: new Array[8][8]
+        }
+    },
+    mounted: function(){
+
+    }
+}
+</script>
+
+
+<style>
+/* taken from https://stackoverflow.com/questions/26432492/chessboard-html5-only/26432909 */
+    .chess-board { border-spacing: 0; border-collapse: collapse; }
+    .chess-board th { padding: .5em; }
+    .chess-board td { border: 1px solid; width: 2em; height: 2em; }
+    .chess-board .light { background: #eee; }
+    .chess-board .dark { background: #000; }
+</style>
---
+++ src/components/Piece.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="piece">
+      <img :src="'@/assets/' + pieceType + '.png'" v-on:click="selected" />
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pieceType: {
+            type: String
+        },
+        space: {
+            type: Array
+        }
+    },
+    data: function() {
+        return{
+            hasNotMoved: true,
+        }
+    }, 
+    methods: {
+        selected: function(){
+            this.$emit('pieceSelected', this.space)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
