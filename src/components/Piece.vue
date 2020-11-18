864ed45f4a60b8c28028498c2aac8eade80ee090
Michael Papadopoulos <micpap25@gmail.com>
2020-11-18 16:33:58 -0500

    More comments

--- src/components/Piece.vue
+++ src/components/Piece.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="piece">
+      <!-- Load the piece's image, which is stored in assets, -->
+      <!-- Based on the name that was passed from Board on creation. -->
+      <!-- Set up a handler for when it is clicked. -->
       <img :src="require(`../assets/${pieceType}.png`)" v-on:click="selected" />
   </div>
 </template>
@@ -15,11 +18,13 @@ export default {
         }
     },
     data: function() {
+        //Hold variables that the piece itself contains. (Doesn't matter yet but good to know about.)
         return{
             hasNotMoved: true
         }
     }, 
     methods: {
+        //When the piece is clicked, it sends back the space it is on. 
         selected: function(){
             this.$emit('pieceSelected', this.space)
         }
