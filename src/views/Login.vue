e01756531831e0af3812eeedd37f2182c1cd2872
ajk8703 <ajkim8703@gmail.com>
2020-11-20 00:02:36 -0500

    Added Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="app">
+        <p>HELLOOOOOOO</p>
+        <p>HELLOOOOOOO</p>
+        <span><h1>Enter Game Code Here: </h1><input type="text" placeholder="Game Code"></span>
+
+    </div>
+
+</template>
+
