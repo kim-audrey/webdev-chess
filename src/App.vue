a30d9a82596f34bdd6964e19034a7f8ef11f9574
Michael Papadopoulos <micpap25@gmail.com>
2020-11-20 21:27:02 -0500

    Fixed routing so everything can be present

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link to="/">Login</router-link> |
+      <router-link to="/game">Game</router-link>
+    </div>
     <router-view />
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 
 
@@ -12,15 +11,15 @@ const routes = [
     name: "Login",
     component: Login
   },
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
-  // }
+  {
+    path: "/game",
+    name: "Game",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Game.vue")
+  }
 ];
 
 const router = new VueRouter({
