9535a75828d1197df44875119677ffe4a4bfce88
Alex <adruzenko03@gmail.com>
2020-12-10 14:51:06 -0500

    More minor server changes

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const socketio = require('socket.io')
+
+//set up the server
+const app = express()
+const server = app.listen(process.env.PORT || 8000)
+const io = socketio(server, {
+    cors: {
+      origin: '*',
+    }
+  });
+
+//respond when the client connects
+io.on("connection", (socket) => {
+    socket.on('roomListRequest',()=>{
+      socket.emit("roomListResponse",Array.from(io.sockets.adapter.rooms));
+    })
+    socket.on('joinRoom', (code) => {
+      socket.removeAllListeners("moveEvent");
+      socket.leave(socket.room)
+      var room = io.sockets.adapter.rooms.get(code);
+      if( room!=null && room.size>=2){
+        console.log("fullroom")
+        socket.emit("fullRoom")
+      }
+      else{
+        socket.join(code)
+        socket.room=code
+        room = io.sockets.adapter.rooms.get(code);
+        if(room.size==1){
+          var cf = (Math.random() < 0.5 ? "Black" : "White");
+          socket.emit("setColor",cf);
+          room.FirstColor=cf;
+        } 
+        else if(room.size==2){
+          socket.to(code).emit("getColor");
+          socket.on("giveColor", (col)=>{
+            var cs = (col === "White" ? "Black" : "White");
+            socket.emit("setColor",cs);
+          })
+
+        }
+        socket.on('moveEvent', (data)=>{
+          socket.to(code).emit("moveResponse", data)
+        })
+        socket.on("colorEcho", (col)=>{
+          socket.to(code).emit("colorEcho",col);
+        })
+      }
+    })
+})
--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import VueSocketIOExt from 'vue-socket.io-extended';
 import io from 'socket.io-client';
 
 //get the socket.io server that is running at server.js
-const socket = io('http://localhost:8000');
+const port= process.env.PORT || 8000
+const socket = io('http://localhost:' + port);
 //set up Vue to use that server
 Vue.use(VueSocketIOExt, socket);
 Vue.config.productionTip = false;
