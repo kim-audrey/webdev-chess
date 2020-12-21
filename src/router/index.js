a4c5ba80bc66b8ec9bfe9b8bab6090a4e1e427c3
Alex <adruzenko03@gmail.com>
2020-12-21 11:54:02 -0500

    Added 404 elements

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../views/Login.vue";
+import Game from "../views/Game.vue";
+import NoGameCode from "../views/NoGameCode.vue";
+import NotFound from "../views/NotFound.vue";
 
 
 Vue.use(VueRouter);
@@ -17,9 +20,19 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Game.vue")
-  }
+    component: Game
+  },
+  {
+    path: "/game/",
+    name: "NoGameCode",
+    component: NoGameCode
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound
+  },
+
 ];
 
 const router = new VueRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="cent">
+        <div style="top: 35%">
+
+            <h1>Error 404:</h1>
+            <h2>Page does not exist</h2>
+            <button class="button" @click="$router.push({ path: '/' })">
+                Go Back
+            </button>
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: "NotFound",
+}
+</script>
+
+<style>
+.button {
+  cursor: pointer;
+}
+.cent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+</style>
